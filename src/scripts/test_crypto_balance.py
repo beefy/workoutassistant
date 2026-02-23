aa8e08a1b4549c07db19d99b0beda0a4e1277054
Nate Schultz <nate.schultz@outlook.com>
2026-02-22 22:06:06 -0500

    get crypto balance

---
+++ src/scripts/test_crypto_balance.py
@@ -0,0 +1,28 @@
+
+from solana.rpc.api import Client
+from solders.pubkey import Pubkey
+from solana.rpc.types import TokenAccountOpts
+import os
+
+wallet_address = os.getenv("SOLANA_ADDRESS")
+
+client = Client("https://api.mainnet-beta.solana.com")
+response = client.get_token_accounts_by_owner(
+    Pubkey.from_string(wallet_address),
+    TokenAccountOpts(program_id=Pubkey.from_string(
+        "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"  # SPL Token Program
+    ))
+)
+
+print("response:", response)
+for account in response.value:
+    data = account.account.data.parsed["info"]
+    mint = data["mint"]
+    amount = int(data["tokenAmount"]["amount"])
+    decimals = int(data["tokenAmount"]["decimals"])
+
+    ui_amount = amount / (10 ** decimals)
+
+    print(f"Token mint: {mint}")
+    print(f"Balance: {ui_amount}")
+    print("------")
