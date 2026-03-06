467c405a2801f024aecd323917b0fe5208f6a7c9
Nate Schultz <nate.schultz@outlook.com>
2026-03-05 23:14:08 -0500

    misc

--- src/scripts/ai_voice.py
+++ src/scripts/ai_voice.py
@@ -27,25 +27,24 @@ def clone_voice(ref_audio_path, ref_text, gen_text):
     # )
 
     # TTS to a file, use a preset speaker
-    tts.tts_to_file(
-        text="Hello world!",
-        language="en",
-        speaker="Craig Gutsy",
-        file_path="output.wav"
-    )
+    # tts.tts_to_file(
+    #     text="Hello world!",
+    #     language="en",
+    #     speaker="Craig Gutsy",
+    #     file_path="output.wav"
+    # )
 
     tts.tts_to_file(
         text="Hello world!",
         language="en",
-        source_wav="output.wav",
-        target_wav=ref_audio_path,
+        speaker_wav=ref_audio_path,
         file_path="cloned_output.wav"
     )
 
 
 if __name__ == "__main__":
     clone_voice(
-        ref_audio_path='src/audio_samples/obama.ogg',
+        ref_audio_path='src/audio_samples/obama2.ogg',
         ref_text="Mrs Rodham was a remarkable person, to anyone who knows her history knows what a strong, determined, and gifted person she was. For her to have been able to live the life that she did and to see her daughter succeed at the pinnacle of public service in this country, I'm sure was deeply satisfying to her. You know. My thoughts and Chel's thoughs, the entire white house's thoughts go out to the entire Clinton family and I know that she will be remembered as someone who helped make a difference in this country and this world. Alright?",
         gen_text="Test text to see if voice cloning works!"
     )
